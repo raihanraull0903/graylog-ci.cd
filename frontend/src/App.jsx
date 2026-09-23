4d5bc2b97809e75d4e60f4bcdb191618c6a2c931
Danish Athaya <danish.athayan@gmail.com>
2026-09-23 17:02:39 +0700

    update v1.0.2-beta.1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 // ============================================
 // CONFIG
 // ============================================
-const BACKEND = "http://127.0.0.1:7777";
+const BACKEND = "http://49.0.3.225:9091";
 const HISTORY_VISIBLE_COUNT = 7;
 const CLOCK_TICK_MS = 1000;
 
